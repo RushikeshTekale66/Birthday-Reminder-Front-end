0d2ee4eedc081d5927893b7d069d93f112793c56
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-27 20:09:28 +0530

    Frontend is ok

--- Frontend/src/Component/board.js
+++ Frontend/src/Component/board.js
@@ -4,14 +4,44 @@ import "./board.css"
 
 const Info = [
     {
-        img: "https://www.befunky.com/images/prismic/82e0e255-17f9-41e0-85f1-210163b0ea34_hero-blur-image-3.jpg?auto=avif,webp&format=jpg&width=896",
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
         name: "Rushikesh Tekale",
         birthday: "2001-02-10"
     },
     {
         img: "https://www.befunky.com/images/prismic/82e0e255-17f9-41e0-85f1-210163b0ea34_hero-blur-image-3.jpg?auto=avif,webp&format=jpg&width=896",
-        name: "Rushikesh Tekale",
+        name: "Tekale Sandhya",
         birthday: "2001-3-10"
+    },
+    {
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
+        name: "Adinath Tekale",
+        birthday: "2001-08-10"
+    },
+    {
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
+        name: "Rohan Tekale",
+        birthday: "2004-3-10"
+    },
+    {
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
+        name: "Yash Tekale",
+        birthday: "2008-02-10"
+    },
+    {
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
+        name: "Rushikesh Patil",
+        birthday: "1999-3-10"
+    },
+    {
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
+        name: "Akash Shinde",
+        birthday: "2001-02-10"
+    },
+    {
+        img: "https://t3.ftcdn.net/jpg/02/00/90/24/240_F_200902415_G4eZ9Ok3Ypd4SZZKjc8nqJyFVp1eOD6V.jpg",
+        name: "Vaibhav Tambare",
+        birthday: "2005-3-10"
     }
 ]
 
@@ -27,7 +57,7 @@ export default function board() {
             <div className='board'>
                 <List info={Today(Info)} />
                 <h2 className='upcoming text-dark upheading'>Upcomming Birthday</h2>
-                <List info={Upcomming(Info, 2)} upcomming={true} />
+                <List info={Upcomming(Info, 10)} upcomming={true} />
             </div>
         </main>
     )
